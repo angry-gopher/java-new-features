f419384d5a1e6cbe0354fb8ca5b39712ddd74d90
angry-gopher <artem@grabyo.com>
2023-01-15 10:45:52 +0100

    formatting.

--- src/com/github/angrygopher/javanew/java13/TextBlocksPreview.java
+++ src/com/github/angrygopher/javanew/java13/TextBlocksPreview.java
@@ -4,14 +4,15 @@ public class TextBlocksPreview {
     public static void main(String[] args) {
         var before = "{\n" + "name: \"Jon\",\n" + "surname: \"Snow\",\n" + "age: 23,\n" + "city: \"Winterfell\"\n" + "}";
 
-        var after = """                                           
-                {       
-                name: "Jon",                                                  
-                surname: "Snow",                     
-                age: 23,           
-                city: "Winterfell"                     
-                }""";
-        System.out.println(before.equals(after));
+        var after = """
+                {
+                  "name":"Jon",
+                  "surname":"Snow",
+                  "age":23,
+                  "city":"Winterfell"
+                }
+                """;
+
     }
 
 }
