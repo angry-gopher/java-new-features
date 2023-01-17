98d56ccdce9f16ca207fdba28e8c9ff4f0f95efe
angry-gopher <artem@grabyo.com>
2023-01-17 16:10:39 +0100

    fixes

--- src/com/github/angrygopher/javanew/java19/StructuredConcurrencyIncubator.java
+++ src/com/github/angrygopher/javanew/java19/StructuredConcurrencyIncubator.java
@@ -1,4 +1,36 @@
 package com.github.angrygopher.javanew.java19;
 
+//import jdk.incubator.concurrent.StructuredTaskScope;
+//
+//import java.util.concurrent.ExecutionException;
+//import java.util.concurrent.Future;
+
 public class StructuredConcurrencyIncubator {
+
+//    Response handle() throws ExecutionException, InterruptedException {
+//        try (var scope = new StructuredTaskScope.ShutdownOnFailure()) {
+//            Future<String> user = scope.fork(this::findUser);
+//            Future<Integer> order = scope.fork(this::fetchOrder);
+//
+//            scope.join();           // Join both forks
+    //short-circuiting pattern  if one of tasks fails the rest fill be cancelled as well
+//            scope.throwIfFailed();  // ... and propagate errors
+//
+//            // Here, both forks have succeeded, so compose their results
+//            return new Response(user.resultNow(), order.resultNow());
+//        }
+//    }
+
+    record Response(String user, int order) {
+    }
+
+    private String findUser() throws InterruptedException {
+        Thread.sleep(1000);
+        return "user";
+    }
+
+    private int fetchOrder() throws InterruptedException {
+        Thread.sleep(1000);
+        return 1;
+    }
 }
--- src/com/github/angrygopher/javanew/java19/VirtualThreadsPreview1.java
+++ src/com/github/angrygopher/javanew/java19/VirtualThreadsPreview1.java
@@ -5,11 +5,20 @@ import java.util.concurrent.Executors;
 import java.util.stream.IntStream;
 
 public class VirtualThreadsPreview1 {
+//    A virtual thread is an instance of java.lang.Thread that runs Java code on an underlying OS thread
+//    but does not capture the OS thread for the code's entire lifetime.
+//    This means that many virtual threads can run their Java code on the same OS thread, effectively sharing it.
+//    While a platform thread monopolizes a precious OS thread, a virtual thread does not.
+//    The number of virtual threads can be much larger than the number of OS threads.
+//    Go has goroutines
+
     public static void main(String[] args) {
+        // compare with ThreadPoolExecutor(no thread pools!)
         try (var executor = Executors.newVirtualThreadPerTaskExecutor()) {
             IntStream.range(0, 10_000).forEach(i -> {
                 executor.submit(() -> {
                     Thread.sleep(Duration.ofSeconds(1));
+                    System.out.println(i);
                     return i;
                 });
             });
