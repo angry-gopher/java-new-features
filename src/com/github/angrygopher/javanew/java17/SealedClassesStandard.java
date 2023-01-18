ce9e85babc4081b924727da704a12f053a04cf95
angry-gopher <artem@grabyo.com>
2023-01-18 14:49:38 +0100

    small

--- src/com/github/angrygopher/javanew/java17/SealedClassesStandard.java
+++ src/com/github/angrygopher/javanew/java17/SealedClassesStandard.java
@@ -1,19 +1,6 @@
 package com.github.angrygopher.javanew.java17;
 
 public class SealedClassesStandard {
-
-    static abstract sealed class Planet permits Earth, Mars {
-
-    }
-
-    static final class Earth extends Planet {
-
-    }
-
-    static final class Mars extends Planet {
-
-    }
-
-
-
+    //java 15 &
+    //java 16 previews are standard now
 }
