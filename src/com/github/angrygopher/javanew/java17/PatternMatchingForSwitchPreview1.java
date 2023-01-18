bb616237a0ae9a81525d6e47254270e3875b2b48
angry-gopher <artem@grabyo.com>
2023-01-18 14:48:41 +0100

    small

--- src/com/github/angrygopher/javanew/java17/PatternMatchingForSwitchPreview1.java
+++ src/com/github/angrygopher/javanew/java17/PatternMatchingForSwitchPreview1.java
@@ -2,9 +2,13 @@ package com.github.angrygopher.javanew.java17;
 
 public class PatternMatchingForSwitchPreview1 {
 
+    public static void main(String[] args) {
+        System.out.println(formatterPatternSwitch(1L));
+    }
+
     static String formatterPatternSwitch(Object o) {
         return switch (o) {
-            case null -> "";
+            case null -> "it was null";
             case Integer i -> String.format("int %d", i);
             case Long l -> String.format("long %d", l);
             case Double d -> String.format("double %f", d);
